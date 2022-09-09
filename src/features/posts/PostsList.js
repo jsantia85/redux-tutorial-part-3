93a40decc4aa75d03300a93ba5bce025b26bc254
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 14:57:58 -0400

    add notes to PostList.js

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export const PostsList = () => {
+  // useSelector is what allows us to communicate with the store
   const posts = useSelector(state => state.posts)
 
+  // this constant is maping through the posts in the store and creating the template for what the posts will include
   const renderedPosts = posts.map(post => (
     <article className='post-exerpt' key={post.id}>
       <h3>{post.title}</h3>
@@ -12,6 +14,7 @@ export const PostsList = () => {
   ))
 
   return (
+    // finally it is returned as a list
     <section className='posts-list'>
       <h2>Posts</h2>
       {renderedPosts}
