35b8307eebd6c529c75ed2a7a32c4fcccec8ae72
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 18:08:53 -0400

    stub up form section for title

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -6,10 +6,21 @@ export const AddPostForm = () => {
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
-  
+
   return (
     <>
-    
+      <section>
+        <form>
+          <label htmlFor='postTitle'>Post Title:</label>
+          <input
+            type='text'
+            id='postTitle'
+            name='postTitle'
+            value={title}
+            onChange={onTitleChanged}
+          />
+        </form>
+      </section>
     </>
   )
 }
