d7a30cf58eeda2c3174422ba9bb37788d9738ebb
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 17:59:12 -0400

    create functions to set the state for title and content when user inputs

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -4,7 +4,9 @@ export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
-
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+  
   return (
     <>
     
