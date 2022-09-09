9a7374d296c6718b052eaa0c0868a74c38f6d2fc
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 14:36:33 -0400

    create mock state to render ui

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,2 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const initialState = [
+  {id: '1', title: 'First Post!', content: 'Hello!'},
+  {id: '2', title: 'Second Post', content: 'More text'},
+]
+
