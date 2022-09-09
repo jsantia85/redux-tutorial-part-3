41102fdcddf91b0b0af829158bd0800f8ad99303
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 17:53:52 -0400

    stub up initial state for title and content

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 
 export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
 
   return (
     <>
