4c062af96767398d2132bf5348b31782a8cd0497
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-09 14:56:31 -0400

    complete the rest of the component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,5 +4,17 @@ import { useSelector } from 'react-redux'
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
 
+  const renderedPosts = posts.map(post => (
+    <article className='post-exerpt' key={post.id}>
+      <h3>{post.title}</h3>
+      <p className='post-content'>{post.content.substring(0,100)}</p>
+    </article>
+  ))
 
+  return (
+    <section className='posts-list'>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
 }
